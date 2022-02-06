f49ab8434c9711c1e38c179d314b0698df32cceb
adielstudent12 <91585240+adielstudent12@users.noreply.github.com>
2022-02-06 15:35:58 -0600

    Update Vps-Mx-8.5x.sh

--- Vps-Mx-8.5x.sh
+++ Vps-Mx-8.5x.sh
@@ -206,7 +206,7 @@ cd
 msg -ama "               Finalizando Instalacion" && msg bar2
 [[ $(find /etc/VPS-MX/controlador -name nombre.log | grep -w "nombre.log" | head -1) ]] || wget -O /etc/VPS-MX/controlador/nombre.log https://raw.githubusercontent.com/adielstudent12/Script-Vps-2022/main/nombre.log &>/dev/null
 [[ $(find /etc/VPS-MX/controlador -name IDT.log | grep -w "IDT.log" | head -1) ]] || wget -O /etc/VPS-MX/controlador/IDT.log https://github.com/lacasitamx/VPSMX/raw/master/ArchivosUtilitarios/IDT.log &>/dev/null
-[[ $(find /etc/VPS-MX/controlador -name tiemlim.log | grep -w "tiemlim.log" | head -1) ]] || wget -O /etc/VPS-MX/controlador/tiemlim.log https://github.com/lacasitamx/VPSMX/raw/master/ArchivosUtilitarios/tiemlim.log &>/dev/null
+[[ $(find /etc/VPS-MX/controlador -name tiemlim.log | grep -w "tiemlim.log" | head -1) ]] || wget -O /etc/VPS-MX/controlador/tiemlim.log https://raw.githubusercontent.com/adielstudent12/Script-Vps-2022/main/tiemlim.log &>/dev/null
 touch /usr/share/lognull &>/dev/null
 wget https://raw.githubusercontent.com/adielstudent12/Script-Vps-2022/main/SPR -O /usr/bin/SPR &>/dev/null &>/dev/null
 chmod 775 /usr/bin/SPR &>/dev/null
